355d6a786357ee45a99a6985ff5bfb7d36a3eb86
ShaneN <shanen@silversalmon.com>
2016-01-11 13:28:59 -0700

    Initial check in of layout tests

---
+++ XamLayout20/LayoutHost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamLayout20
+{
+    public class LayoutHost : Layout<View>
+    {
+        protected override bool ShouldInvalidateOnChildAdded(View child)
+        {
+            return false; // stop pestering me
+        }
+
+        protected override bool ShouldInvalidateOnChildRemoved(View child)
+        {
+            return false; // go away and leave me alone
+        }
+
+        protected override void OnChildMeasureInvalidated()
+        {
+            // I'm ignoring you.  You'll take whatever size I want to give
+            // you.  And you'll like it.
+        }
+
+
+        Rectangle theRectangle;
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            theRectangle = new Rectangle(x, y, width, height);
+            LayoutTheThings();
+        }
+
+        void LayoutTheThings()
+        {
+            var theWidth = theRectangle.Width / Children.Count();
+            var theX = theRectangle.X;
+
+            foreach (View v in Children)
+            {
+                Rectangle r = new Rectangle(theX, theRectangle.Y, theWidth, theRectangle.Height);
+                v.Layout(r);
+
+                theX += theWidth;
+            }
+        }
+    }
+}
---
+++ XamLayout20/MainPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamLayout20
+{
+    public class MainPage : ContentPage
+    {
+        Label theLabel;
+        public MainPage()
+        {
+            Button clickMe = new Button()
+            {
+                Text = "Click Me"
+            };
+
+            theLabel = new Label
+            {
+                XAlign = TextAlignment.Center,
+                Text = "Welcome to Xamarin Forms!"
+            };
+
+
+            clickMe.Clicked += ClickMe_Clicked;
+            Content = new LayoutHost
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                        theLabel,
+                        clickMe
+                    }
+            };
+        }
+
+        private void ClickMe_Clicked(object sender, EventArgs e)
+        {
+            theLabel.Text += "x";
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            base.LayoutChildren(x, y, width, height);
+            Debug.WriteLine("Layout Changed");
+        }
+    }
+}
